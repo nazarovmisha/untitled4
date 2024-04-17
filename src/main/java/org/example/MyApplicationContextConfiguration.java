2defd1b48f9a79f8206fcd16f48c5ee2066681aa
https://github.com/nazarovmisha <nazarovmisha@inbox.ru>
2024-04-17 14:37:30 +0300

    Start

--- src/main/java/org/example/MyApplicationContextConfiguration.java
+++ src/main/java/org/example/MyApplicationContextConfiguration.java
@@ -1,2 +1,25 @@
-package org.example;public class MyApplicationContextConfiguration {
+package org.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class MyApplicationContextConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUser("root");
+        dataSource.setPassword("s3cr3t");
+        dataSource.setURL("jdbc:mysql://localhost:3306/myDatabase");
+        return dataSource;
+    }
+
+    @Bean
+    public UserDao userDao() { // (1)
+        return new UserDao(dataSource());
+    }
+
 }
